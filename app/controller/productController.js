185091b3f4b944292a6c9bd8516f060817d6c930
Iftikhar-hub <iftikharifti1673@gmail.com>
2026-01-29 21:02:24 +0500

    Payment Feauture Implemened

--- app/controller/productController.js
+++ app/controller/productController.js
@@ -1,4 +1,4 @@
-
+  
 
 const productModel = require('../models/productModel')
 
--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.use(userRouter);
 
 
 connectDB().then(() => {
+    
     app.listen(process.env.PORT || 8000, () => {
         console.log("Server is running on Port:", process.env.PORT || 8000);
     });
