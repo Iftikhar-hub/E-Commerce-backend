29a151e991cab8832fff5ce3df735be4dba7047f
Iftikhar-hub <iftikharifti1673@gmail.com>
2026-01-29 21:17:48 +0500

    Payment Feauture Implemened

--- app/middleware/authMiddleware.js
+++ app/middleware/authMiddleware.js
@@ -5,6 +5,8 @@ const authMiddleware = async (req, res, next) => {
     
     try {
         const token = req.cookies.token; 
+        console.log('Auth Token:', token);
+
         if (!token) {
             return res.status(400).json({ msg: "Unauthorized Access" });
         }
