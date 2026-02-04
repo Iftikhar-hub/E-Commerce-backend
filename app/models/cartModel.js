c079eb27463ab64908275d7d7389adc9f2cea95e
Iftikhar-hub <iftikharifti1673@gmail.com>
2026-02-04 11:50:53 +0500

    CORS Problem Resolved

--- app/models/cartModel.js
+++ app/models/cartModel.js
@@ -4,7 +4,7 @@ const cartSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true, unique: true },
     items: [
         {
-            productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+            productId: { type: mongoose.Schema.Types.ObjectId, ref: 'product', required: true },
             quantity: { type: Number, default: 1 },
         }
     ]
