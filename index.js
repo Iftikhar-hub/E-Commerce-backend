216f0e3225c1255d8de0fbd2b6095ada298477d2
Iftikhar-hub <iftikharifti1673@gmail.com>
2026-02-13 18:18:37 +0500

    WishList Problem solved

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(cookieParser());
 
 app.use(cors({
     origin: "https://e-commerce-nu-five-82.vercel.app",
-    origin:'http://localhost:5173',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -38,9 +37,9 @@ app.use(userRouter);
 
 connectDB().then(() => {
     
-    app.listen(process.env.PORT || 8000, () => {
-        console.log("Server is running on Port:", process.env.PORT || 8000);
-    });
+    // app.listen(process.env.PORT || 8000, () => {
+    //     console.log("Server is running on Port:", process.env.PORT || 8000);
+    // });
 });
 
 module.exports = app;
